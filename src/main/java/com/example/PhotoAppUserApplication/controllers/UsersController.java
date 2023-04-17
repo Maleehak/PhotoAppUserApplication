5c25d8524f6e0d98770d2ee2d8f102e89d770fa1
Maleeha Khalid <maleehakhalid604@gmail.com>
2023-04-17 23:44:32 +0500

    spring cloud bus
    It does not work. Adding it for reference

--- src/main/java/com/example/PhotoAppUserApplication/controllers/UsersController.java
+++ src/main/java/com/example/PhotoAppUserApplication/controllers/UsersController.java
@@ -8,6 +8,7 @@ import jakarta.validation.Valid;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
+@RefreshScope
 public class UsersController {
 
   @Autowired
